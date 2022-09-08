ca7286b6288d95e5cba1b9de16ae2a0a2e610c7d
Ashutosh Krishna <ashutosh.krishna@thoughtworks.com>
2022-09-08 16:51:55 +0530

    [Ashutosh] Add. function to repeat character n times

--- Iteration/repeat.go
+++ Iteration/repeat.go
@@ -1,8 +1,8 @@
 package iteration
 
-func Repeat(ch string) string {
+func Repeat(ch string, repeatCount int) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
+	for i := 0; i < repeatCount; i++ {
 		repeated += ch
 	}
 	return repeated
--- Iteration/repeat_test.go
+++ Iteration/repeat_test.go
@@ -3,16 +3,31 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("\nActual: %q\nExpected: %q", repeated, expected)
+	t.Run("should repeat characters 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+
+		assertCorrectMessage(t, repeated, expected)
+	})
+
+	t.Run("should repeat characters 8 times", func(t *testing.T) {
+		repeated := Repeat("a", 8)
+		expected := "aaaaaaaa"
+
+		assertCorrectMessage(t, repeated, expected)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, wanted string) {
+	t.Helper()
+	if got != wanted {
+		t.Errorf("\nActual: %q\nExpected: %q", got, wanted)
 	}
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", b.N)
 	}
 }
