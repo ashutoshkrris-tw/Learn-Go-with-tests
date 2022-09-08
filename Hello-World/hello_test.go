d7f03f553443af30a7c784b27e403cfe8f7f80b8
Ashutosh Krishna <ashutosh.krishna@thoughtworks.com>
2022-09-08 15:26:22 +0530

    [Ashutosh] Add. greet 'Hello, World' when empty string is passed as name

--- Hello-World/hello_test.go
+++ Hello-World/hello_test.go
@@ -3,9 +3,24 @@ package main
 import "testing"
 
 func TestHelloUser(t *testing.T) {
-	got := Hello("Ashutosh")
-	wanted := "Hello, Ashutosh"
 
+	t.Run("should say hello to users", func(t *testing.T) {
+		got := Hello("Ashutosh")
+		wanted := "Hello, Ashutosh"
+
+		assertCorrectMessage(t, got, wanted)
+	})
+
+	t.Run("should say 'Hello, World' when an empty string is passed", func(t *testing.T) {
+		got := Hello("")
+		wanted := "Hello, World"
+
+		assertCorrectMessage(t, got, wanted)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got string, wanted string) {
+	t.Helper()
 	if got != wanted {
 		t.Errorf("\nActual: %q\nExpected: %q", got, wanted)
 	}
