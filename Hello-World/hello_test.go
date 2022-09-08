c409c300f93b652f81f741db544b00def993b237
Ashutosh Krishna <ashutosh.krishna@thoughtworks.com>
2022-09-08 15:56:10 +0530

    [Ashutosh] Add. multiple language support for greet function

--- Hello-World/hello_test.go
+++ Hello-World/hello_test.go
@@ -5,15 +5,36 @@ import "testing"
 func TestHelloUser(t *testing.T) {
 
 	t.Run("should say hello to users", func(t *testing.T) {
-		got := Hello("Ashutosh")
+		got := Hello("Ashutosh", "")
 		wanted := "Hello, Ashutosh"
 
 		assertCorrectMessage(t, got, wanted)
 	})
 
 	t.Run("should say 'Hello, World' when an empty string is passed", func(t *testing.T) {
-		got := Hello("")
+		got := Hello("", "")
 		wanted := "Hello, World"
+
+		assertCorrectMessage(t, got, wanted)
+	})
+
+	t.Run("should greet in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		wanted := "Hola, Elodie"
+
+		assertCorrectMessage(t, got, wanted)
+	})
+	
+	t.Run("should greet in French", func(t *testing.T) {
+		got := Hello("Elodie", "French")
+		wanted := "Bonjour, Elodie"
+
+		assertCorrectMessage(t, got, wanted)
+	})
+
+	t.Run("should greet in Hindi", func(t *testing.T) {
+		got := Hello("Elodie", "Hindi")
+		wanted := "Hello, Elodie"
 
 		assertCorrectMessage(t, got, wanted)
 	})
