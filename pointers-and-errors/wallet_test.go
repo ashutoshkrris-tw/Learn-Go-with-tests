bed816468eb6e5f55daaa68ac624990239c2e1b0
Ashutosh Krishna <ashutosh.krishna@thoughtworks.com>
2022-09-09 22:37:38 +0530

    [Ashutosh] Add. withdraw methods in wallet

--- pointers-and-errors/wallet_test.go
+++ pointers-and-errors/wallet_test.go
@@ -3,12 +3,28 @@ package pointers_and_errors
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(100)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(100))
+
+		assertBalance(t, wallet, Bitcoin(100))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(500)}
+
+		wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, Bitcoin(400))
+	})
+}
+
+func assertBalance(t testing.TB, wallet Wallet, wanted Bitcoin) {
+	t.Helper()
 
 	got := wallet.Balance()
-	wanted := Bitcoin(100)
 
 	if got != wanted {
 		t.Errorf("\nActual: %s\nExpected: %s", got, wanted)
