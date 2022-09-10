485baa713062206801be4852b5b10f325dd6a380
Ashutosh Krishna <ashutosh.krishna@thoughtworks.com>
2022-09-10 14:26:51 +0530

    [Ashutosh] Add. add method to add in dictionary

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -19,6 +19,29 @@ func TestSearch(t *testing.T) {
 	})
 }
 
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{"Hello": "World"}
+
+	key := "NewWord"
+	value := "Added"
+	dictionary.Add(key, value)
+
+	assertDefinition(t, dictionary, key, value)
+}
+
+func assertDefinition(t testing.TB,dictionary Dictionary, key, value string) {
+	t.Helper()
+
+	got, err := dictionary.Search(key)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	if got != value {
+		t.Errorf("\nActual: %q\nExpected: %q", got, value)
+	}
+}
+
 func assertEqual(t testing.TB, got, wanted string) {
 	t.Helper()
 	if got != wanted {
